641d4607e7f138f5122964aeed1cc0b4caeca7ba
Muhammad Salman <m.salman86@gmail.com>
2016-11-21 11:38:57 -0500

    added aurelia webapp and moved backend to api folder

---
+++ webapp/README.md
@@ -0,0 +1,15 @@
+# Dotsub Assignment Webapp
+A simple webapp that lets user upload files with some metadata (title, description and creation date), view files' metadata
+with pagination and download a single file. Technology stack is Aurelia framework with Javascript ES6/7
+
+## Requirements
+- node & npm
+- npm install -g aurelia-cli
+
+## Getting Started
+1. `au build`
+2. `au run`
+
+## Notes
+
+1. Webapp runs on port `6005`.
---
+++ webapp/aurelia_project/tasks/build.js
@@ -0,0 +1,35 @@
+import gulp from 'gulp';
+import transpile from './transpile';
+import processMarkup from './process-markup';
+import processCSS from './process-css';
+import {build} from 'aurelia-cli';
+import project from '../aurelia.json';
+
+// FIXME https://github.com/aurelia/cli/issues/248
+
+export default gulp.series(
+  // copySplashStyle,
+  readProjectConfiguration,
+  gulp.parallel(
+    transpile,
+    processMarkup,
+    processCSS
+  ),
+  writeBundles
+);
+
+function copySplashStyle(done) {
+  var splashCss = './' + project.paths.root + '/styles/initial.css';
+  var dest = './' + project.build.targets[0].output;
+  gulp.src(splashCss).pipe(gulp.dest(dest));
+  done();
+}
+
+function readProjectConfiguration() {
+  return build.src(project);
+}
+
+function writeBundles() {
+  console.log(project.platform);
+  return build.dest();
+}
